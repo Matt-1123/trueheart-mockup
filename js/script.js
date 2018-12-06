a58aad11c8bb35449c3edd2669afba31dd7502ab
ray-vazquez <herschel.rey@gmail.com>
2018-12-06 18:23:55 -0500

    adding bars

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// 
+//
+//     var images =['https://picsum.photos/1400/499','https://picsum.photos/1400/500','https://picsum.photos/1400/501'];
+//
+// var swapImage = document.getElementById('jumbotron');
+// var counter = 0;
+//
+// function changeImage() {
+//     swapImage.style.background = images[counter];
+//     counter ++;
+//
+// }
+//
+// setInterval(changeImage, 500);
