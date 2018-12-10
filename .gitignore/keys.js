c8f29d943fbac89b48d76b80d553314fad069877
Ramon Vazquez <herschel.rey@gmail.com>
2018-12-10 17:23:32 -0500

    Removed gitignore files wireframed what-we-do.html.  moved jquery script from volunteer.html to script.js

--- .gitignore/keys.js
+++
@@ -1,2 +0,0 @@
-var instagramClientId= '00e6d5f377a6498b916e85a54c011c0f';
-var instgramClientSecret = '520fb1810ac742ca93de4c749a9139a6';
--- js/script.js
+++ js/script.js
@@ -22,3 +22,34 @@ $(function(){
     mobileNav.slideToggle(200).toggleClass('show');
   });
 });
+
+$(function(){
+
+  var navToggle = $('.nav-toggle');
+  var nav = $('#nav');
+  var navItem = $('#nav > li');
+  var subNav = $('.sub-nav'); // ul nested in navItem
+
+  navToggle.on('click', function(){
+    navToggle.toggleClass("fa-times fa-bars");
+    nav.slideToggle(200).toggleClass('show');
+
+    // to-do: close all open subnavs
+  });
+
+  $(navItem).on('click', function(){
+    var el = $(this);
+    el.children('.sub-nav').slideToggle(200).toggleClass('show');
+    // toggle up and down arrow on nav item
+    setTimeout(function() {
+      el.toggleClass('expanded');
+    }, 200);
+
+    // close other open subnavs
+    // if(el.siblings().children('sub-nav').hasClass("show")){
+    //   el.siblings().children('sub-nav')
+    //     .removeClass("show");
+    //     .slideToggle(400);
+    // }
+  });
+});
